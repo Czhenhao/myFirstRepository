3d5bff4da8d37e8b175c84cbcb71a527d0151427
Czhenhao <827895386@qq.com>
2020-09-27 16:46:24 +0800

    Servlet Improve

---
+++ JavaWeb/CRUD/src/com/echo/controller/CategoriesServlet.java
@@ -0,0 +1,55 @@
+package com.echo.controller;
+
+import com.echo.util.ExecuteUtil;
+import com.echo.util.JDBCUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.sql.Connection;
+
+public class CategoriesServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String servletPath = request.getServletPath();
+        if ("/add".equals(servletPath)) add(request,response);
+        else if ("/delete".equals(servletPath)) delete(request,response);
+        else if ("/update".equals(servletPath)) update(request,response);
+    }
+
+    private void update(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+        Integer categoryid = Integer.parseInt(request.getParameter("categoryid"));
+        String categoryname = request.getParameter("categoryname");
+        String description = request.getParameter("description");
+        boolean update = ExecuteUtil.update(categoryid, categoryname, description);
+        if (update) response.getWriter().print("更改成功");
+        else response.getWriter().print("更改失败");
+    }
+
+    private void delete(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.setCharacterEncoding("utf-8");
+        response.setContentType("text/html;charset=utf-8");
+        boolean index = ExecuteUtil.delete(Integer.parseInt(request.getParameter("index")));
+        if (index) response.getWriter().print("删除成功!");
+        else response.getWriter().print("删除失败!");
+    }
+
+    private void add(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+        String categoryName = request.getParameter("CategoryName");
+        String description = request.getParameter("Description");
+        Connection connection = JDBCUtil.getConnection();
+        boolean add = ExecuteUtil.add(categoryName, description);
+        if (add) response.getWriter().print("添加成功！");
+        else response.getWriter().print("添加失败");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+}
